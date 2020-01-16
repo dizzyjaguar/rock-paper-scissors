648d44ce20cb8c6b686874c4382a667cb28ea53a
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-16 10:25:48 -0800

    get all the functions working, still missing tests, and css

---
+++ app.js
@@ -0,0 +1,39 @@
+import { getRandomThrow, checkResult } from './get-random-throw.js'; 
+
+const winsShow = document.getElementById('wins-span');
+const lossesShow = document.getElementById('losses-span');
+const drawsShow = document.getElementById('draws-span');
+const userChoice = document.getElementById('user-choice');
+const npcChoice = document.getElementById('npc-choice');
+const results = document.getElementById('results');
+const battleButton = document.getElementById('battle-button');
+
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
+battleButton.addEventListener('click', () =>{
+    const humanInput = document.querySelector('input:checked');
+    const humanChoice = humanInput.value;
+    const npcChose = getRandomThrow();
+    const result = checkResult(humanChoice, npcChoice);
+    
+    userChoice.textContent = humanChoice;
+    npcChoice.textContent = npcChose;
+
+
+    if (result === 'win') {
+        wins++;
+        winsShow.textContent = wins;
+        results.textContent = 'Nice one! You won this battle!';
+    } else if (result === 'lose') {
+        losses++;
+        lossesShow.textContent = losses;
+        results.textContent = 'Ah damn! NPC Knight got ya, you lost this one';
+    } else {
+        draws++;
+        drawsShow.textContent = draws;
+        results.textContent = 'You were equally matched';
+    }
+});
+
